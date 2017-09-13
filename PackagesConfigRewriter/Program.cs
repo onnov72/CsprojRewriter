109964deda69ac84e68637f839eefef682573a27
Onno Veersma <oveersma@diskad.nl>
2017-09-13 16:41:07 +0700

    Implement

--- PackagesConfigRewriter/Program.cs
+++ PackagesConfigRewriter/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace PackagesConfigRewriter
 {
@@ -10,6 +12,36 @@ namespace PackagesConfigRewriter
     {
         static void Main(string[] args)
         {
+            string fileName = args[0];//Path.Combine(Directory.GetCurrentDirectory(), "packages.config");
+            if (File.Exists(fileName))
+            {
+                XDocument document = XDocument.Load(fileName);
+                var elements = document.Root.Elements("package").ToList();
+                elements.Reverse();
+                elements.ForEach(x => Change(x));
+                document.Root.Name = "ItemGroup";
+                document.Save(fileName);
+            }
+        }
+
+        private static void Change(XElement element)
+        {
+            element.Name = "PackageReference";
+            foreach (XAttribute attribute in element.Attributes().Reverse().ToList())
+            {
+                if (attribute.Name == "id")
+                {
+                    element.SetAttributeValue("Include", attribute.Value);
+                }
+                else
+                {
+                    if(attribute.Name == "version")
+                    {
+                        element.SetAttributeValue("Version", attribute.Value);
+                    }
+                }
+                attribute.Remove();
+            }
         }
     }
 }
